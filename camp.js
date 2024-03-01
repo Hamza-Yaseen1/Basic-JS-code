cff99f144a43a381750e4f6b32d822810ab00a15
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2024-03-01 15:08:34 +0500

    if -else condition used

---
+++ camp.js
@@ -0,0 +1,10 @@
+let username = prompt("Enter username :");
+let pass = prompt("Enter Password :");
+ 
+if (username =="hamza" && pass ==  "1234") {
+    alert ("Welcome Hamza!");
+}
+else{
+    alert("wrong  username or password");
+}
+
